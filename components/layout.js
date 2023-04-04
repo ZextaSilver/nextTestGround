363ce5cc41488822cc10d9fcd07b8627f9958348
bowenzhao <bzhao19@ucsc.edu>
2023-04-03 22:59:58 -0400

    changed background

--- components/layout.js
+++ components/layout.js
@@ -9,71 +9,26 @@ export const siteTitle = '| Bowen Zhao';
 
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <link rel="icon" href="../images/kirby.png" />
       </Head>
-      <div className = {styles.topnav}>
+      
+      <header className={styles.header}>
         <Link href = "/">Main Page</Link>
         <Link href = "/posts/aboutMe">About Me</Link>
-      </div>
-      {/* <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt=""
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
-          </>
-        )}
-      </header> */}
+      </header>
+
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
           <Link href = "/"> ← Back to home</Link>
         </div>
       )}
-      <div className = {styles.botnav}>
+
+      <footer className = {styles.footer}>
         <Link href = "https://www.linkedin.com/in/bowen-zhao-656743218/">LinkedIn</Link>
-      </div>
-    </div>
+      </footer>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,52 @@ export default function Home() {
         </Head>
         <section className = {utilStyles.headingMd}>
           <p>
-            Welcome, good to see you here
+            1
+          </p>
+          <p>
+            2
+          </p>
+          <p>
+            3
+          </p>
+          <p>
+            4
+          </p>
+          <p>
+            5
+          </p>
+          <p>
+            6
+          </p>
+          <p>
+            7
+          </p>
+          <p>
+            8
+          </p>
+          <p>
+            9
+          </p>
+          <p>
+            10
+          </p>
+          <p>
+            11
+          </p>
+          <p>
+            12
           </p>
           <p>
             <Link href = "/posts/aboutMe">More About Me?</Link>
           </p>
+          <p>
+            13
+          </p>
+          <p>
+            14
+          </p>
+          
+          
         </section>
       </div>
     </Layout>
