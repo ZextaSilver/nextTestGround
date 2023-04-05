0f909c3cd5d960c289c798ee156150ab87652e56
bowenzhao <bzhao19@ucsc.edu>
2023-04-04 21:01:20 -0400

    footer at bottom of the page, added copyright info

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
 import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
@@ -14,8 +13,7 @@ export const siteTitle = '| Bowen Zhao';
 export default function Layout({ children, home }) {
   return (
     
-    <>
-      <main>{children}
+    <div className={styles.container}>
 
         <Head>
           <link rel="icon" href="../images/kirby.png" />
@@ -25,14 +23,18 @@ export default function Layout({ children, home }) {
           <Link href = "/">Main Page</Link>
           <Link href = "/posts/projects">Projects</Link>
         </header>
+        
+      <main>{children}</main>
 
-        <footer className = {styles.footer}>
+      <footer className={styles.footer}>
+      
           <Link href = "https://www.linkedin.com/in/bowen-zhao-656743218/"><FontAwesomeIcon icon={faLinkedin} size='2x'/></Link>
           <Link href = "https://github.com/ZextaSilver"><FontAwesomeIcon icon={faGithub} size='2x'/></Link>
           <Link href = "mailto:zbw1020@gmail.com"><FontAwesomeIcon icon={faEnvelope} size='2x'/></Link>
-        </footer>
+          <p>Copyright ©{new Date().getFullYear()} BowenZhao.</p>
 
-      </main>
-    </>
+      </footer>
+
+    </div>
   );
 }
